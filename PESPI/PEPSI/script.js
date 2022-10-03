c23ab92e948b0625f01c66041cf627d4bb13d1d4
Satyam singh <satyamsingh2003a@gmail.com>
2022-10-03 15:04:19 +1000

    Add files via upload

---
+++ PESPI/PEPSI/script.js
@@ -0,0 +1,7 @@
+var imgSlider = (anything) =>{
+    document.querySelector('.pepsi').src = anything;
+}  
+function changeBgColor(color) {
+    var sec = document.querySelector('.sec');
+    sec.style.background=color;
+}
